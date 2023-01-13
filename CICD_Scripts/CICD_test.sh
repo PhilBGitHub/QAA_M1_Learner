744be163c0a2122f455ce162e0040c710a35b181
PhilBGitHub <122531546+PhilBGitHub@users.noreply.github.com>
2023-01-13 14:24:16 +0000

    Update CICD_test.sh

---
+++ CICD_Scripts/CICD_test.sh
@@ -0,0 +1,8 @@
+!# /bin/bash
+
+FileNum = ls | wc -1
+if [FileNum > 5]; then echo "Repo has more than 5 files!"; else echo "Repo is very small! :("
+
+echo "Last code pushed on" >> /CICD_Scripts/accessLog.txt
+echo date >> /CICD_Scripts/accessLog.txt
+echo "by "&id >> /CICD_Scripts/accessLog.txt
